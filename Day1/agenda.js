70dc0e68db73f4896e742c3dcf71f82f8bfee93b
thetechdope <91004778+thetechdope@users.noreply.github.com>
2022-06-30 14:25:05 +0530

    Day 1 Material

---
+++ Day1/agenda.js
@@ -0,0 +1,15 @@
+ /*
+
+    Day 1 - 29th June 2022
+
+    Today's Agenda -
+
+    A brief Introduction to JavaScript
+    History of JavaScript
+    Linking a JavaScript File
+    Writing a Simple JavaScript Program
+    Is JavaScript Compiled or Interpreted?
+    Identifiers    
+    Basics of HTML & CSS
+
+ */
---
+++ Day2/agenda.js
@@ -0,0 +1,16 @@
+ /*
+
+   Day 2 - 30th June 2022
+
+   Today's Agenda -
+
+   What are Data Types in JavaScript?
+   Working with different Data Types - Boolean, Number, String, Array, etc
+   Basic Operators
+   Operator Precedence
+   Hoisting (variables and functions)
+   Different types of identifiers - Let, const & var
+   Temporal Dead Zone
+   Undefined v/s not defined
+
+ */
