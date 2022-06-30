4f17bd94f3106cb491aae1a79244e157d4c75680
thetechdope <91004778+thetechdope@users.noreply.github.com>
2022-06-30 14:19:23 +0530

    Day1 Material

---
+++ Day1/index.js
@@ -0,0 +1,16 @@
+// Identifiers or Variables
+
+var favouriteColor = "Green";
+console.log(favouriteColor);
+
+var favourite01 = "Red";
+var Favourite01 = "Orange";
+
+var $favourite01 = "Purple";
+
+console.log(favourite01);
+console.log(Favourite01);
+console.log($favourite01);
+
+var favouriteSubject; // Variable Declaration
+favouriteSubject = "Maths"; // Variable Initalisation
