55287631d148530a8c8af6ed565fa6a6a577fd8d
thetechdope <91004778+thetechdope@users.noreply.github.com>
2022-07-19 16:01:06 +0530

    Day 13 Material

---
+++ Day13/agenda.js
@@ -0,0 +1,15 @@
+/*
+
+  Today's Agenda -
+
+  Asynchronous JavaScript
+  Callbacks in JS
+  Promises
+  Handling Rejected Promises
+  Chaining Promises
+  Consuming Promises with Async/Await
+  Error Handling with try/catch
+  Running multiple promises in Parallel
+  Promise combinators - Promise.all, Promise.race, Promise.allSettled and Promise.any
+      
+ */
