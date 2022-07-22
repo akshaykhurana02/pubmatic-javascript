829b6ddcab918be5dbd28aa1dc4737277c123d5d
Akshay Khurana <khakshay@athenahealth.com>
2022-07-22 14:09:19 +0530

    Day 14 Material

--- Day13/agenda.js
+++ Day13/agenda.js
@@ -6,14 +6,5 @@
   Async vs defer
   How is HTML Parsed
   Callbacks in JS
-
-  
-  Promises
-  Handling Rejected Promises
-  Chaining Promises
-  Consuming Promises with Async/Await
-  Error Handling with try/catch
-  Running multiple promises in Parallel
-  Promise combinators - Promise.all, Promise.race, Promise.allSettled and Promise.any
       
  */
---
+++ Day15/agenda.js
@@ -0,0 +1,8 @@
+/*
+
+  Today's Agenda -
+
+  Running multiple promises in Parallel
+  Promise combinators - Promise.all, Promise.race, Promise.allSettled and Promise.any
+  Higher Order Functions - map, reduce & filter
+ */
